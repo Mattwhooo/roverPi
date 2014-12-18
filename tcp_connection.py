8b8b2bce812a34885e85fbaad705d2c3c0ebce22
Matt Watson <Matt.Watson@orasi.com>
2014-12-18 14:17:25 -0500

    some changes

--- tcp_connection.py
+++ tcp_connection.py
@@ -1,18 +1,23 @@
 import threading
 import socket
-
+import os
 
 
 class TCPStream(object):
-
+    type = 'tcp'
     def __init__(self, host, port, size=1024, backlog=5):
         self.host = host
         self.port = port
         self.size = size
         self.backlog = backlog
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.host, self.port))
-        self.server.listen(200)
+
+        if self.type == 'tcp':
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.bind((self.host, self.port))
+            self.server.listen(200)
+        if self.type == 'udp':
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.server.bind(('', self.port))
 
     def open(self):
         print ('Waiting For Stream Connection...')
@@ -23,7 +28,6 @@ class TCPStream(object):
         self.server.shutdown(1)
 
 class CommandStream(TCPStream):
-
     def run(self):
 
         while True:
@@ -36,40 +40,57 @@ class CommandStream(TCPStream):
                     return False
                 elif data == '<GAMEPADINFO>':
                     cntrls = ControlStream(self.host, self.port + 1)
+                    self.control = cntrls
                     self.client.send(str(self.host) + ':' + str(self.port + 1))
                     cntrls.open()
                     print('Control Stream Loaded!')
+                elif data == '<GAMEPADKILL>':
+                    self.control.stop_thread()
+                elif data == '<VIDEOINFO>':
+                    os.system('raspivid -t 0 -h 720 -w 1080 -fps 25 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=' + self.host + ' port=' + str(self.port + 2))
                 else:
                     self.client.send('...')
 
 class ControlStream(TCPStream):
-    def __init__(self, host, port, size=1024, backlog=5):
-        super(ControlStream, self).__init__(host, port, size, backlog)
-        self.server = self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    type = 'udp'
 
     def open(self):
         print ('Opening Control Stream...')
+        self.stop = False
         #super(ControlStream, self).open()
         thread = threading.Thread(target=self.run, args=())
         thread.daemon = True
         thread.start()
 
-    def run(self):
+    def stop_thread(self):
+        self.stop = True
+        self.server.close()
 
+    def run(self):
+        self.server.settimeout(1)
         while True:
-            data = self.server.recvfrom(1024)
-            if data:
-                print 'Control-Stream: ' + data
+            try:
+                data, addr = self.server.recvfrom(1024)
+                print 'Control-Stream: ',  data
+            except:
+                pass
+
+            if self.stop:
+                break
+        print 'Exit GamePad Thread'
 
 if __name__ =='__main__':
-    CS = CommandStream('192.168.1.144', 5000, 1024, 5)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("gmail.com",80))
+    ip = s.getsockname()[0]
+    s.close()
+    CS = CommandStream(ip, 5002)
     while True:
         CS.open()
         cont = CS.run()
         if cont:
             pass
-            print 'cont'
         else:
             CS.close()
-            print 'exit1'
+            print 'Releasing Command Stream Connection'
 
